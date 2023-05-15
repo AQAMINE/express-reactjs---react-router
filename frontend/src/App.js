f53e0bf6a87d3e58b7d890bb2913afae6fb5f720
aqamine <32686488+AQAMINE@users.noreply.github.com>
2023-05-15 02:36:34 +0100

    move function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/Home";
-import EventsPage from './pages/Events';
+import EventsPage, {loader as eventsLoader} from './pages/Events';
 import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
@@ -14,16 +14,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       {path: '/events', element: <EventsRootLayout />, children:[
-        { index: true, element: <EventsPage />, loader: async () => {
-          const response = await fetch('http://localhost:8080/events');
-
-          if (!response.ok) {
-            //field to load
-          } else {
-              const resData = await response.json();
-              return resData.events;
-          }
-        }},
+        { index: true, element: <EventsPage />, loader: eventsLoader },
         { path: '/events/:eventId', element: <EventDetailPage /> },
         { path: '/events/new', element: <NewEventPage /> },
         { path: '/events/:eventId/edit', element: <EditEventPage />}
