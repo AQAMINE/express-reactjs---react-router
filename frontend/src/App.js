ca6258328fb7b294e744b67c42a041c18f30cbb1
aqamine <32686488+AQAMINE@users.noreply.github.com>
2023-05-24 00:49:11 +0100

    useRouteLoaderData

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import RootLayout from "./pages/Root";
 import HomePage from "./pages/Home";
 import EventsPage, {loader as eventsLoader} from './pages/Events';
-import EventDetailPage from './pages/EventDetail';
+import EventDetailPage, {loader as eventDetailLoader} from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import EventsRootLayout from "./pages/EventsRoot";
@@ -16,11 +16,21 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
+
       {path: '/events', element: <EventsRootLayout />, children:[
         { index: true, element: <EventsPage />, loader: eventsLoader },
-        { path: '/events/:eventId', element: <EventDetailPage /> },
-        { path: '/events/new', element: <NewEventPage /> },
-        { path: '/events/:eventId/edit', element: <EditEventPage />}
+
+        //to share data loaded between routes
+        { path: ':eventId', 
+        id: 'event-detail',
+        loader: eventDetailLoader,
+        children: [
+          { index: true , element: <EventDetailPage />},
+          { path: 'edit', element: <EditEventPage />}
+        ] },
+
+        { path: 'new', element: <NewEventPage /> },
+        
       ]}
     ]
   }
